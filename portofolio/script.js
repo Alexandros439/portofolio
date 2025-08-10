87c0fbc29b8e43a603c8c7327ee3211e5a8fa009
Alexandros439 <hondoalexandros@gmail.com>
2025-08-10 20:20:49 +0300

    Add files via upload

---
+++ portofolio/script.js
@@ -0,0 +1,14 @@
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
+
+
+window.addEventListener("contextmenu",function(e){
+    e.preventDefault()
+})
+  
